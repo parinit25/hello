3a1808d4651d8f34f9ec7436f6bd48b413ddf50f
PArinit <parinit.singh06@gmail.com>
2022-07-08 19:26:30 +0530

    Task 8 edit and delete

---
+++ task8.js
@@ -0,0 +1,58 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+//Form submit event //
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click',removeItem);
+
+// Add item 
+function addItem(e)
+   {
+    e.preventDefault();
+  // get input value 
+  var newItem = document.getElementById('item').value;
+
+  //create new li element
+  var li = document.createElement('li');
+  li.className = 'list-group-item';
+
+  //Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  var deleteBtn = document.createElement('button');
+
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  li.appendChild(deleteBtn);
+
+  itemList.appendChild(li);
+}
+function removeItem(e)
+{
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+var editBtn = document.createElement('button');
+
+
+editBtn.textContent = 'Click me';
+
+editBtn.className="btn btn-sm float-right delete";
+
+var btnList = document.getElementsByClassName("list-group-item");
+
+for(var k =0 ;k<btnList.length;k++)
+{
+    btnList[k].appendChild(editBtn);
+}
+// li.appendChild(editBtn);
+
+// itemList.appendChild(li);
+
+
